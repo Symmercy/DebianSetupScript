97acae2e0d3bb002426aa1c52570d934f117184b
BloodRaven123 <91673840+BloodRaven123@users.noreply.github.com>
2022-07-07 12:08:11 +0200

    Update DebianSetup.sh

--- DebianSetup.sh
+++ DebianSetup.sh
@@ -5,13 +5,11 @@ sudo apt upgrade
 sudo apt install apt-transport-https curl thunar htop neofetch nano vim kate arandr virt-manager sddm wget timeshift xorg kitty vlc
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sleep (2)
 sudo apt update
 sudo apt install brave-browser
 sudo systemctl enable sddm
 sudo systemctl enable xorg
 sudo wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
-sleep (3)
 sudo dpkg -i steam
 sudo wget https://sejda-cdn.com/downloads/sejda-desktop_7.5.2_amd64.deb
 sudo dpkg -i sejda-desktop_7.5.2_amd64.deb
