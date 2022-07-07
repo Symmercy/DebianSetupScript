4c1d03e5ab4c292c226bd727d4ab2bda20fa630f
BloodRaven123 <91673840+BloodRaven123@users.noreply.github.com>
2022-07-07 11:16:36 +0200

    Update DebianSetup.sh

--- DebianSetup.sh
+++ DebianSetup.sh
@@ -2,7 +2,12 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install htop neofetch nano vim kate arandr virt-manager sddm curl wget timeshift xorg
+sudo apt install apt-transport-https curl thunar htop neofetch nano vim kate arandr virt-manager sddm wget timeshift xorg
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sleep (2)
+sudo apt update
+sudo apt install brave-browser
 sudo systemctl enable sddm
 sudo systemctl enable xorg
 sudo wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
