ef46820755831d6fe70dcd75627e1fe55b9b301d
BloodRaven123 <91673840+BloodRaven123@users.noreply.github.com>
2022-07-07 11:41:38 +0200

    Update DebianSetup2.sh

--- DebianSetup2.sh
+++ DebianSetup2.sh
@@ -2,4 +2,5 @@
 
 #/bin/bash
 
-sudo apt remove sweeper okular kmail konqueror juk k3b xterm
+sudo apt remove sweeper okular kmail konqueror juk k3b xterm libreoffice libreoffice-writer libreoffice-calc libreoffice-draw libreoffice-math
+  
