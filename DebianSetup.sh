d52d7fb107241f093c0888ed2945a5c5c2451a85
BloodRaven123 <91673840+BloodRaven123@users.noreply.github.com>
2022-07-07 11:44:47 +0200

    Update DebianSetup.sh

--- DebianSetup.sh
+++ DebianSetup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install apt-transport-https curl thunar htop neofetch nano vim kate arandr virt-manager sddm wget timeshift xorg kitty
+sudo apt install apt-transport-https curl thunar htop neofetch nano vim kate arandr virt-manager sddm wget timeshift xorg kitty vlc
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sleep (2)
