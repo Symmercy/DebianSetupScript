375956929b7a3014ba86db6d8929bcecc03614bb
Symmercy <91673840+Symmercy@users.noreply.github.com>
2023-02-18 10:12:28 +0100

    Update DebianSetup2.sh

--- DebianSetup2.sh
+++ DebianSetup2.sh
@@ -1,5 +1,3 @@
-#This part of the script is deinstalling software that i don't use from kde plasma and in general.
-
 #/bin/bash
 
 sudo apt remove sweeper okular kmail konqueror juk k3b xterm libreoffice libreoffice-writer libreoffice-calc libreoffice-draw libreoffice-math kwrite knotes kmouth kcalc konsole korganizer
